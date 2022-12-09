d700430df036df48ce450e4522b475403573ec9b
Irina Burada <irina.burada01@gmail.com>
2022-12-10 00:41:13 +0200

    for ex 2 extract triples from rdf file

--- ontology.py
+++ ontology.py
@@ -1,6 +1,14 @@
-def main():
-    return
+import rdflib
 
+g = rdflib.Graph()
 
-if __name__ == "__main__":
-    main()
+g.parse("food.rdf", format='application/rdf+xml')
+
+for subject, predicate, obj in g.triples((None, None, None)):
+    if not isinstance(subject, rdflib.term.BNode) and \
+            not isinstance(obj, rdflib.term.BNode) and \
+            not isinstance(predicate, rdflib.term.BNode):
+        subject = str(subject).split('#')[-1]
+        predicate = str(predicate).split('#')[-1]
+        obj = str(obj).split('#')[-1]
+        print(subject, " is in relation ", predicate, " with ", obj)
