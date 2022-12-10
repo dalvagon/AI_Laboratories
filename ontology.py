a920f2de9827248543e35a7cea8cd253a55490cc
Irina Burada <irina.burada01@gmail.com>
2022-12-10 14:08:14 +0200

    add 2 types of questions and validate answer

--- ontology.py
+++ ontology.py
@@ -16,10 +16,9 @@ for subject, predicate, obj in g.triples((None, None, None)):
 
         # ex 2
         print(subject, " is in relation ", predicate, " with ", obj)
-
-        # ex 3
         relations.append((subject, predicate, obj))
 
+# ex 3
 # generate questions
 
 while True:
@@ -27,14 +26,24 @@ while True:
     answer = input()
     if answer == 'n':
         break
-    else :
+    else:
+        type_of_question = random.randint(1, 2)
         pos = random.randint(0, len(relations) - 1)
-        print("What is the relation between ", relations[pos][0], " and ", relations[pos][2], "?")
-        answer = input()
-        for triple in relations:
-            if triple[0] == relations[pos][0] and triple[2] == relations[pos][2]:
-                if triple[1] == answer:
-                    print("Correct!")
-                else:
-                    print("Incorrect! The correct answer is ", triple[1])
-
+        if type_of_question == 1:
+            print("What is the relation between ", relations[pos][0], " and ", relations[pos][2], "?")
+            answer = input()
+            for triple in relations:
+                if triple[0] == relations[pos][0] and triple[2] == relations[pos][2]:
+                    if triple[1] == answer:
+                        print("Correct!")
+                    else:
+                        print("Incorrect! The correct answer is ", triple[1])
+        else:
+            print("Who is related to ", relations[pos][0], " with ", relations[pos][1], " relation?")
+            answer = input()
+            for triple in relations:
+                if triple[0] == relations[pos][0] and triple[1] == relations[pos][1]:
+                    if triple[2] == answer:
+                        print("Correct!")
+                    else:
+                        print("Incorrect! The correct answer is ", triple[2])
